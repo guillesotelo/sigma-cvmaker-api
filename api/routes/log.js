9fb3d7828313bb5a96946e4908a17805e3023702
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2022-12-16 11:35:40 +0100

    updated log

--- api/routes/log.js
+++ api/routes/log.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { Log, User} = require('../db/models')
+const { Log, User } = require('../db/models')
 
 //Get all logs
 router.get('/getAll', async (req, res, next) => {
@@ -14,6 +14,28 @@ router.get('/getAll', async (req, res, next) => {
 
             res.status(200).json(logs)
         } else res.status(403).send('User does not have the required permission')
+    } catch (err) {
+        console.error('Something went wrong!', err)
+        res.send(500).send('Server Error')
+    }
+})
+
+//Create new Log
+router.post('/create', async (req, res, next) => {
+    try {
+        const { email, username, details, module, itemId } = req.body
+
+        const log = await Log.create({
+            username,
+            email,
+            details,
+            module,
+            itemId: itemId || null
+        })
+
+        if (!log) return res.status(400).json('Error creating Log')
+
+        res.status(200).json(log)
     } catch (err) {
         console.error('Something went wrong!', err)
         res.send(500).send('Server Error')
